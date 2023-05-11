72511ab0b280410bd832a81f4f5927aa49d0b886
Fernanda Acuña <fdafilgueira@gmail.com>
2023-05-11 17:39:30 -0400

    muestra boton borrar en todos los post

--- src/components/Muro.js
+++ src/components/Muro.js
@@ -1,7 +1,7 @@
 // import { onNavigate } from "./components/main"
 
 import { doc } from "firebase/firestore/lite";
-import { addpost, exit, listarPublicaciones } from "../lib/auth";
+import { addpost, borrarTexto, exit, listarPublicaciones, authStateChangedEvent } from "../lib/auth";
 import { onNavigate } from "../lib/router";
 import { addDoc, onSnapshot, orderBy, startAt } from "firebase/firestore";
 
@@ -30,79 +30,130 @@ export function muro() {
     // contenedorMuro.appendChild(logoMuro); MOSTRADO ABAJO
 
     // input del texto/post
+    //function AreaPost() {
+    const contenedorAreaPost = document.createElement("section");
+    contenedorMuro.appendChild(contenedorAreaPost);
+
     const areaDelPost = document.createElement("textarea");
-    const lugarDelPost = document.getElementById("registrateAqui");
-    areaDelPost.classList = "areaDelPost";
-    areaDelPost.placeholder = "Escribe aqui..."
+    //const lugarDelPost = document.getElementById("registrateAqui");
+    areaDelPost.className = "areaDelPost";
+    areaDelPost.placeholder = "Escribe aqui...";
+    areaDelPost.id = "areaDelPost";
 
-    
-    //contenedorMuro.appendChild(areaDelPost); MOSTRADO ABAJO
-
-    // //contenedor de 3 botones
-    const contenedorBtns = document.createElement("section");
-    contenedorBtns.innerHTML = `<button id="botonEliminar">Eliminar</button>`
-    `<button id="botonEditar>"Editar"</button>`
-    // contenedorBtns.classlist = "contenedorBtns";
-    // contenedorMuro.appendChild(contenedorBtns);
-    // // contenedorBtns.appendChild(botonPost) 
-
-    //CREACION DEL BOTON EDITAR(aun sin css)
-    // const botonEditar = document.createElement("button");
-    // botonEditar.classList = "btnEditar";
-    // botonEditar.textContent = "Editar";
-    // //CREACION DEL BOTON ELIMINAR(aun sin css)
-    // const botonEliminar = document.createElement("button");
-    // botonEliminar.classList = "btnEliminar";
-    // botonEditar.textContent = "Eliminar";
 
     //CREACION DEL BOTON PUBLICAR
     const botonPost = document.createElement("button");
-    botonPost.type = "submit";
+    botonPost.id = "botonPost";
     botonPost.classList = "btnPublicar";
     botonPost.textContent = "Publicar";
+    botonPost.setAttribute("type", "submit")
     //contenedorMuro.appendChild(botonPost); MOSTRADO ABAJO
     botonPost.addEventListener('click', () => {
         const publicacion = areaDelPost.value;
         addpost(publicacion)
         console.log(addpost.publicacion)
-        contenedorBtns.appendChild(botonPost) 
-
     });
+
+    contenedorMuro.appendChild(areaDelPost);
+    //  };
+
+
+    // //CREACION DEL BOTON PUBLICAR
+    // const botonPost = document.createElement("button");
+    // botonPost.type = "submit";
+    // botonPost.classList = "btnPublicar";
+    // botonPost.textContent = "Publicar";
+    // //contenedorMuro.appendChild(botonPost); MOSTRADO ABAJO
+    // botonPost.addEventListener('click', () => {
+    //     const publicacion = areaDelPost.value;
+    //     addpost(publicacion)
+    //     console.log(addpost.publicacion)
+    //     //contenedorBtns.appendChild(botonPost)
+
+    // });
+
+    //  }
 
     //llamar a la funcion en firestore, desde auth, INNERHTML<<<
     //contenedor para mostrar los post creados
     const contenedorPosts = document.createElement("section");
     contenedorPosts.classList.add("contenedorPosts");
 
-        listarPublicaciones((resultado) => {
-            resultado.forEach(element => {
-                const showPostList = document.createElement("article");
-                showPostList.classList = "listaDePost";
 
-                //con los template literal puedo mostrar el texto y el email
-                showPostList.textContent = `${element.data().text} - ${element.data().email}`;
-                //console.log(element.data());
-         
-                
-                contenedorPosts.appendChild(showPostList);
-              
+    // listarPublicaciones((resultado) => {
+    //     resultado.forEach(element => {
+    //         const showPostList = document.createElement("article");
+    //         showPostList.classList = "listaDePost";
 
+    //         //con los template literal puedo mostrar el texto y el email
+    //         showPostList.textContent = `${element.data().text} -Publicado por ${element.data().email}`;
+    //         //console.log(element.data());
+    //         contenedorPosts.appendChild(showPostList);
+    //     });
+    // });
+
+    listarPublicaciones((resultado) => {
+        resultado.forEach((element) => {
+          // Verificar si el autor del post es el usuario actual
+          if (element.data().userId === authStateChangedEvent.currentUser) {
+            // Crear el artículo que mostrará el texto del post
+            const showPostList = document.createElement("article");
+            showPostList.classList = "listaDePost";
+            showPostList.textContent = `${element.data().text} -Publicado por ${element.data().email}`;
+      
+            // Crear el botón de "Eliminar"
+            const deleteButton = document.createElement("button");
+            deleteButton.textContent = "Eliminar";
+            deleteButton.addEventListener("click", () => {
+              // Llamar a la función para eliminar el post
+              borrarTexto(element.id)
+                .then(() => {
+                  console.log("El post ha sido eliminado correctamente");
+                })
+                .catch((error) => {
+                  console.error("Error al eliminar el post:", error);
+                });
             });
+      
+            // Agregar el botón al artículo
+            showPostList.appendChild(deleteButton);
+      
+            // Agregar el artículo al contenedor de posts
+            contenedorPosts.appendChild(showPostList);
+          }
         });
+      });
+      
+
+       // if (postEmail === currentUserEmail) {
+            //     // el post fue publicado por el usuario actual, agregar el botón de eliminar
+            //     const botonEliminar = document.createElement("button");
+            //     botonEliminar.classList = "botonEliminar";
+            //     botonEliminar.textContent = "Eliminar";
+            //     botonEliminar.addEventListener("click", () => {
+            //         eliminarPost(element.text);
+            //         showPostList.remove();
+            //     });
+            //     showPostList.appendChild(botonEliminar);
+           // }
+
+     
 
 
 
-
-
-    //});
     contenedorMuro.appendChild(btnExit);
     contenedorMuro.appendChild(logoMuro);
-    contenedorMuro.appendChild(areaDelPost);
+    contenedorMuro.appendChild(contenedorAreaPost);
+    contenedorAreaPost.appendChild(areaDelPost);
+    contenedorAreaPost.appendChild(botonPost);
+    //contenedorMuro.appendChild(areaDelPost);
     //contenedorMuro.appendChild(botonPost);
     contenedorMuro.appendChild(contenedorPosts);
-    contenedorMuro.appendChild(contenedorBtns);
-   
+    //contenedorMuro.appendChild(contenedorBtns);
+    //contenedorPosts.appendChild(showPostList);
     //contenedorMuro.appendChild(contenedorPosts);
+
+
 
     return contenedorMuro
 }
